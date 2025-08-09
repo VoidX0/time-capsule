e7a99d790c4acf572858b6ddef49dafb6aefe621
VoidX0 <2982927629@qq.com>
2025-08-09 22:30:47 +0800

    Set default appearance to 'system' in theme provider

--- web/src/components/theme-provider.tsx
+++ web/src/components/theme-provider.tsx
@@ -7,5 +7,6 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const appearance = 'system'
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
