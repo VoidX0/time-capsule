8dcb0e4e3fc0d6b26d82add7d5ab914c81545b63
VoidX0 <2982927629@qq.com>
2025-08-12 21:01:02 +0800

    添加摄像头和视频片段管理控制器，更新视频控制器描述

---
+++ api/TimeCapsule/Controllers/SegmentController.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Options;
+using Serilog;
+using SqlSugar;
+using SqlSugar.IOC;
+using TimeCapsule.Core.Models.Db;
+using TimeCapsule.Models;
+using TimeCapsule.Models.Options;
+using ILogger = Serilog.ILogger;
+
+namespace TimeCapsule.Controllers;
+
+/// <summary>
+/// 视频片段管理
+/// </summary>
+[ApiController]
+[DisplayName("视频片段管理")]
+[Route("[controller]/[action]")]
+[TypeFilter(typeof(AllowAnonymousFilter))]
+public class SegmentController : OrmController<VideoSegment>
+{
+}
--- api/TimeCapsule/Controllers/VideoController.cs
+++ api/TimeCapsule/Controllers/VideoController.cs
@@ -2,28 +2,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Options;
-using Serilog;
 using SqlSugar;
 using SqlSugar.IOC;
 using TimeCapsule.Core.Models.Db;
 using TimeCapsule.Models;
 using TimeCapsule.Models.Options;
-using ILogger = Serilog.ILogger;
 
 namespace TimeCapsule.Controllers;
 
 /// <summary>
-/// 视频管理
+/// 视频流管理
 /// </summary>
 [ApiController]
-[DisplayName("视频管理")]
+[DisplayName("视频流管理")]
 [Route("[controller]/[action]")]
 [TypeFilter(typeof(AllowAnonymousFilter))]
 public class VideoController : ControllerBase
 {
     private readonly SystemOptions _systemOptions;
     private readonly ISqlSugarClient _db = DbScoped.SugarScope;
-    private ILogger Logger => Log.ForContext<VideoController>();
 
     /// <summary>
     /// 构造函数
