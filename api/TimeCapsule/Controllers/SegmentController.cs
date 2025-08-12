51234917ecc822703f04ecb5a86cd1ed8879f122
VoidX0 <2982927629@qq.com>
2025-08-12 21:37:43 +0800

    删除segment时，删除对应的文件

--- api/TimeCapsule/Controllers/SegmentController.cs
+++ api/TimeCapsule/Controllers/SegmentController.cs
@@ -58,4 +58,58 @@ public class SegmentController : OrmController<VideoSegment>
         var fileStream = new FileStream(video, FileMode.Open, FileAccess.Read, FileShare.Read);
         return File(fileStream, "image/jpeg", enableRangeProcessing: true);
     }
+
+    #region ORM
+
+    /// <summary>
+    /// 删除数据
+    /// </summary>
+    /// <param name="entity">实例列表</param>
+    /// <returns></returns>
+    [HttpDelete]
+    public override async Task<ActionResult<int>> Delete(List<VideoSegment> entity)
+    {
+        var delete = await base.Delete(entity);
+        if (delete.Result is not OkObjectResult) return delete;
+        var cameraId = entity.Select(x => x.CameraId).Distinct().ToList();
+        var cameras = await Db.Queryable<Camera>().Where(x => cameraId.Contains(x.Id)).ToListAsync();
+        // 删除对应的文件 (删除失败的文件，会在下次Sync时重新同步，所以不需要处理异常)
+        foreach (var segment in entity)
+        {
+            var camera = cameras.FirstOrDefault(x => x.Id == segment.CameraId);
+            if (camera == null) continue;
+            var video = new FileInfo(Path.Combine(_systemOptions.CameraPath, camera.BasePath, segment.Path));
+            var thumbnail = new FileInfo(Path.Combine(_systemOptions.CachePath, segment.CameraId.ToString(),
+                $"{segment.Id}.jpg"));
+            // 删除视频文件
+            if (video.Exists)
+            {
+                try
+                {
+                    video.Delete();
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+
+            // 删除缩略图
+            if (thumbnail.Exists)
+            {
+                try
+                {
+                    thumbnail.Delete();
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+        }
+
+        return delete;
+    }
+
+    #endregion
 }
