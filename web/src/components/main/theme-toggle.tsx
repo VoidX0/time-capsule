e1b6636a2d5950fb03c3071860e0cabb9044eeaa
VoidX0 <2982927629@qq.com>
2025-08-13 15:56:51 +0800

    添加主题切换功能，支持视图过渡动画，优化按钮引用

--- web/src/components/main/theme-toggle.tsx
+++ web/src/components/main/theme-toggle.tsx
@@ -4,22 +4,53 @@ import { Moon, Sun, TvMinimal } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { useTranslations } from 'next-intl'
+import { useRef } from 'react'
+import { flushSync } from 'react-dom'
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme()
   const t = useTranslations('Common')
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  const changeTheme = async (newTheme: 'dark' | 'light' | 'system') => {
+    if (!buttonRef.current) return
+
+    await document.startViewTransition(() => {
+      flushSync(() => {
+        setTheme(newTheme)
+      })
+    }).ready
+
+    const { top, left, width, height } =
+      buttonRef.current.getBoundingClientRect()
+    const y = top + height / 2
+    const x = left + width / 2
+
+    const right = window.innerWidth - left
+    const bottom = window.innerHeight - top
+    const maxRad = Math.hypot(Math.max(left, right), Math.max(top, bottom))
+
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${maxRad}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 700,
+        easing: 'ease-in-out',
+        pseudoElement: '::view-transition-new(root)',
+      },
+    )
+  }
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="mr-2">
+        <Button ref={buttonRef} variant="outline" size="icon" className="mr-2">
           {theme === 'dark' ? (
             <Moon />
           ) : theme === 'light' ? (
@@ -30,15 +61,15 @@ export function ThemeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
+        <DropdownMenuItem onClick={() => changeTheme('light')}>
           <Sun />
           {t('light')}
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
+        <DropdownMenuItem onClick={() => changeTheme('dark')}>
           <Moon />
           {t('dark')}
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
+        <DropdownMenuItem onClick={() => changeTheme('system')}>
           <TvMinimal />
           {t('system')}
         </DropdownMenuItem>
