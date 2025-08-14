4281aa683e36e96b343a2050b43548cb6cf91c84
VoidX0 <2982927629@qq.com>
2025-08-14 23:24:21 +0800

    完成manage segments页面

--- web/src/app/[locale]/(main)/[camera]/playback/page.tsx
+++ web/src/app/[locale]/(main)/[camera]/playback/page.tsx
@@ -83,171 +83,168 @@ export default function Page({
   }
   // 初始化完成
   return (
-    <div className="md:p-8">
+    <div className="mx-auto grid max-w-6xl gap-4 rounded-xl md:p-8">
       <h1 className="mb-6 text-3xl font-bold">{cameraInfo?.Name || ''}</h1>
-      {/*自适应高度*/}
-      <div className="mx-auto grid max-w-6xl gap-4 rounded-xl">
-        {/*播放器*/}
-        <CameraPlayer
-          ref={playerRef}
-          cameraId={cameraInfo?.Id?.toString() ?? ''}
-          initialStartTime={initialTime}
-          onPlayProgress={(ts) => {
-            setProgress(ts) // 更新进度
-            timelineRef.current?.seekTo(ts) // 同步到时间轴
-          }}
-          onStartTimeChange={() => {
-            if (!playerRef.current?.videoElement?.playbackRate) return
-            playerRef.current.videoElement.playbackRate = playbackRate
-          }}
-        />
-        {/*播放器控制*/}
-        <div className="bg-muted/50 rounded-xl">
-          <div className="flex flex-wrap items-center justify-center gap-2 p-4">
-            <Calendar />
-            <p>{new Date(progress).toLocaleString()}</p>
-          </div>
-          <div className="flex flex-wrap items-center justify-center gap-10 p-4">
-            {/*基础控制*/}
-            <div className="flex items-center gap-2">
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => {
-                  if (playerRef.current?.videoElement?.paused) {
-                    playerRef.current?.videoElement?.play().catch(console.warn)
-                  } else {
-                    playerRef.current?.videoElement?.pause()
-                  }
-                }}
-              >
-                {playerRef.current?.videoElement?.paused ? (
-                  <CirclePlay />
-                ) : (
-                  <CirclePause />
-                )}
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() =>
-                  playerRef.current?.videoElement?.requestFullscreen()
+      {/*播放器*/}
+      <CameraPlayer
+        ref={playerRef}
+        cameraId={cameraInfo?.Id?.toString() ?? ''}
+        initialStartTime={initialTime}
+        onPlayProgress={(ts) => {
+          setProgress(ts) // 更新进度
+          timelineRef.current?.seekTo(ts) // 同步到时间轴
+        }}
+        onStartTimeChange={() => {
+          if (!playerRef.current?.videoElement?.playbackRate) return
+          playerRef.current.videoElement.playbackRate = playbackRate
+        }}
+      />
+      {/*播放器控制*/}
+      <div className="bg-muted/50 rounded-xl">
+        <div className="flex flex-wrap items-center justify-center gap-2 p-4">
+          <Calendar />
+          <p>{new Date(progress).toLocaleString()}</p>
+        </div>
+        <div className="flex flex-wrap items-center justify-center gap-10 p-4">
+          {/*基础控制*/}
+          <div className="flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => {
+                if (playerRef.current?.videoElement?.paused) {
+                  playerRef.current?.videoElement?.play().catch(console.warn)
+                } else {
+                  playerRef.current?.videoElement?.pause()
                 }
-              >
-                <Fullscreen />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => {
-                  if (playerRef.current?.videoElement == null) return
-                  playerRef.current.videoElement.muted =
-                    !playerRef.current.videoElement.muted
-                }}
-              >
-                {playerRef.current?.videoElement?.muted ? (
-                  <VolumeOff />
-                ) : (
-                  <Volume2 />
-                )}
-              </Button>
-              {playerRef.current?.videoElement?.muted ? null : (
-                <Slider
-                  className="w-48"
-                  defaultValue={[0]}
-                  max={100}
-                  step={1}
-                  onValueChange={(value) => {
-                    if (!playerRef.current?.videoElement) return
-                    playerRef.current.videoElement.volume = value[0]! / 100
-                  }}
-                />
+              }}
+            >
+              {playerRef.current?.videoElement?.paused ? (
+                <CirclePlay />
+              ) : (
+                <CirclePause />
               )}
-            </div>
-            {/*快进快退*/}
-            <div className="flex items-center gap-2">
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() =>
-                  playerRef.current?.seekTo(progress - 3 * 24 * 60 * 60 * 1000)
-                }
-              >
-                <ChevronsLeft />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() =>
-                  playerRef.current?.seekTo(progress - 24 * 60 * 60 * 1000)
-                }
-              >
-                <ChevronLeft />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() =>
-                  playerRef.current?.seekTo(progress + 24 * 60 * 60 * 1000)
-                }
-              >
-                <ChevronRight />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() =>
-                  playerRef.current?.seekTo(progress + 3 * 24 * 60 * 60 * 1000)
-                }
-              >
-                <ChevronsRight />
-              </Button>
-            </div>
-            {/*倍速*/}
-            <div className="flex items-center gap-2">
-              <Button
-                size="icon"
-                variant={playbackRate === 1 ? 'outline' : 'ghost'}
-                onClick={() => setPlaybackRate(1)}
-              >
-                X1
-              </Button>
-              <Button
-                size="icon"
-                variant={playbackRate === 4 ? 'outline' : 'ghost'}
-                onClick={() => setPlaybackRate(4)}
-              >
-                X4
-              </Button>
-              <Button
-                size="icon"
-                variant={playbackRate === 8 ? 'outline' : 'ghost'}
-                onClick={() => setPlaybackRate(8)}
-              >
-                X8
-              </Button>
-              <Button
-                size="icon"
-                variant={playbackRate === 16 ? 'outline' : 'ghost'}
-                onClick={() => setPlaybackRate(16)}
-              >
-                X16
-              </Button>
-            </div>
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() =>
+                playerRef.current?.videoElement?.requestFullscreen()
+              }
+            >
+              <Fullscreen />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => {
+                if (playerRef.current?.videoElement == null) return
+                playerRef.current.videoElement.muted =
+                  !playerRef.current.videoElement.muted
+              }}
+            >
+              {playerRef.current?.videoElement?.muted ? (
+                <VolumeOff />
+              ) : (
+                <Volume2 />
+              )}
+            </Button>
+            {playerRef.current?.videoElement?.muted ? null : (
+              <Slider
+                className="w-48"
+                defaultValue={[0]}
+                max={100}
+                step={1}
+                onValueChange={(value) => {
+                  if (!playerRef.current?.videoElement) return
+                  playerRef.current.videoElement.volume = value[0]! / 100
+                }}
+              />
+            )}
+          </div>
+          {/*快进快退*/}
+          <div className="flex items-center gap-2">
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() =>
+                playerRef.current?.seekTo(progress - 3 * 24 * 60 * 60 * 1000)
+              }
+            >
+              <ChevronsLeft />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() =>
+                playerRef.current?.seekTo(progress - 24 * 60 * 60 * 1000)
+              }
+            >
+              <ChevronLeft />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() =>
+                playerRef.current?.seekTo(progress + 24 * 60 * 60 * 1000)
+              }
+            >
+              <ChevronRight />
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() =>
+                playerRef.current?.seekTo(progress + 3 * 24 * 60 * 60 * 1000)
+              }
+            >
+              <ChevronsRight />
+            </Button>
+          </div>
+          {/*倍速*/}
+          <div className="flex items-center gap-2">
+            <Button
+              size="icon"
+              variant={playbackRate === 1 ? 'outline' : 'ghost'}
+              onClick={() => setPlaybackRate(1)}
+            >
+              X1
+            </Button>
+            <Button
+              size="icon"
+              variant={playbackRate === 4 ? 'outline' : 'ghost'}
+              onClick={() => setPlaybackRate(4)}
+            >
+              X4
+            </Button>
+            <Button
+              size="icon"
+              variant={playbackRate === 8 ? 'outline' : 'ghost'}
+              onClick={() => setPlaybackRate(8)}
+            >
+              X8
+            </Button>
+            <Button
+              size="icon"
+              variant={playbackRate === 16 ? 'outline' : 'ghost'}
+              onClick={() => setPlaybackRate(16)}
+            >
+              X16
+            </Button>
           </div>
         </div>
-        {/*时间轴*/}
-        <CameraTimeline
-          ref={timelineRef}
-          cameraId={cameraInfo?.Id?.toString() ?? ''}
-          initialTime={initialTime}
-          onTimeChange={() => playerRef.current?.videoElement?.pause()}
-          onTimeCommit={(ts) => {
-            playerRef.current?.seekTo(ts)
-            playerRef.current?.videoElement?.play().catch(console.warn)
-          }}
-        />
       </div>
+      {/*时间轴*/}
+      <CameraTimeline
+        ref={timelineRef}
+        cameraId={cameraInfo?.Id?.toString() ?? ''}
+        initialTime={initialTime}
+        onTimeChange={() => playerRef.current?.videoElement?.pause()}
+        onTimeCommit={(ts) => {
+          playerRef.current?.seekTo(ts)
+          playerRef.current?.videoElement?.play().catch(console.warn)
+        }}
+      />
     </div>
   )
 }
