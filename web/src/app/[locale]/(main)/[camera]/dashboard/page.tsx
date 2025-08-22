9109fb36c6d71668b43b029aac4c2b5681305956
VoidX0 <2982927629@qq.com>
2025-08-22 09:01:49 +0800

    Add AppSidebarProvider for managing sidebar state with localStorage

--- web/src/app/[locale]/(main)/[camera]/dashboard/page.tsx
+++ web/src/app/[locale]/(main)/[camera]/dashboard/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import {components} from '@/api/schema'
+import { components } from '@/api/schema'
 import CameraChart from '@/components/camera/camera-chart'
-import {Card, CardContent, CardHeader, CardTitle} from '@/components/ui/card'
-import {openapi} from '@/lib/http'
-import {timeSpanToMilliseconds} from '@/lib/time-span'
-import {ArrowUpRight} from 'lucide-react'
-import {useLocale} from 'next-intl'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { openapi } from '@/lib/http'
+import { timeSpanToMilliseconds } from '@/lib/time-span'
+import { ArrowUpRight } from 'lucide-react'
+import { useLocale } from 'next-intl'
 import Link from 'next/link'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 type QueryDto = components['schemas']['QueryDto']
 type Camera = components['schemas']['Camera']
