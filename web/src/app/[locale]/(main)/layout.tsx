e42d26eb6a3787afb3e892ea66db8982ea8cbc34
VoidX0 <2982927629@qq.com>
2025-09-05 10:24:52 +0800

    Add authorization check handling in AppSidebar and MainLayout

--- web/src/app/[locale]/(main)/layout.tsx
+++ web/src/app/[locale]/(main)/layout.tsx
@@ -1,17 +1,25 @@
+'use client'
+
 import { AppSidebar } from '@/components/main/app-sidebar'
 import AppSidebarProvider from '@/components/main/app-sidebar-provider'
 import MainBreadcrumb from '@/components/main/main-breadcrumb'
 import { Separator } from '@/components/ui/separator'
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useCallback, useState } from 'react'
 
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [authorized, setAuthorized] = useState<boolean | null>(null)
+  const handleAuthChecked = useCallback((authorized: boolean) => {
+    setAuthorized(authorized)
+  }, [])
   return (
     <AppSidebarProvider>
-      <AppSidebar />
+      <AppSidebar onAuthChecked={handleAuthChecked} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
@@ -23,7 +31,17 @@ export default function MainLayout({
             <MainBreadcrumb />
           </div>
         </header>
-        {children}
+
+        {/* 授权检查结果 */}
+        {authorized !== true && (
+          <div className="space-y-4 p-4">
+            <Skeleton className="h-6 w-1/3" />
+            <Skeleton className="h-6 w-1/2" />
+            <Skeleton className="h-6 w-full" />
+            <Skeleton className="h-6 w-2/3" />
+          </div>
+        )}
+        {authorized === true && children}
       </SidebarInset>
     </AppSidebarProvider>
   )
