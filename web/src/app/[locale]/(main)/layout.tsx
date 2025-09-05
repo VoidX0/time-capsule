20687a3dd6daa3e6d75e9f2dd007940bfe52d982
VoidX0 <2982927629@qq.com>
2025-09-05 10:31:46 +0800

    Remove skeleton loading placeholders for unauthorized access in MainLayout

--- web/src/app/[locale]/(main)/layout.tsx
+++ web/src/app/[locale]/(main)/layout.tsx
@@ -5,7 +5,6 @@ import AppSidebarProvider from '@/components/main/app-sidebar-provider'
 import MainBreadcrumb from '@/components/main/main-breadcrumb'
 import { Separator } from '@/components/ui/separator'
 import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
-import { Skeleton } from '@/components/ui/skeleton'
 import { useCallback, useState } from 'react'
 
 export default function MainLayout({
@@ -31,16 +30,6 @@ export default function MainLayout({
             <MainBreadcrumb />
           </div>
         </header>
-
-        {/* 授权检查结果 */}
-        {authorized !== true && (
-          <div className="space-y-4 p-4">
-            <Skeleton className="h-6 w-1/3" />
-            <Skeleton className="h-6 w-1/2" />
-            <Skeleton className="h-6 w-full" />
-            <Skeleton className="h-6 w-2/3" />
-          </div>
-        )}
         {authorized === true && children}
       </SidebarInset>
     </AppSidebarProvider>
