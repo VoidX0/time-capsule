6541053faaf3937dd1151483c0edd7e2306232ed
VoidX0 <2982927629@qq.com>
2025-12-02 11:48:44 +0800

    Update project metadata and improve layout configuration

--- docs/src/app/[lang]/(home)/page.tsx
+++ docs/src/app/[lang]/(home)/page.tsx
@@ -1,6 +1,6 @@
-import { Circuit } from '@/components/loader/circuit'
 import { i18n } from '@/lib/i18n'
 import Link from 'next/link'
+import { Dots } from '@/components/loader/dots'
 
 export const generateStaticParams = () =>
   i18n.languages.map((lang) => ({ lang }))
@@ -14,24 +14,23 @@ export default async function HomePage({
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">
-        {' '}
-        {lang === 'en' ? 'Get started' : '快速开始'}
+        {process.env.NEXT_PUBLIC_PROJECT_NAME || ''}
       </h1>
       <p className="text-fd-muted-foreground">
         {lang === 'en'
-          ? 'Get started with our step-by-step guide, explore deployment options, or browse the reference.'
-          : '通过我们的分步指南快速上手，了解部署选项，或浏览参考文档。'}
+          ? process.env.NEXT_PUBLIC_PROJECT_DESCRIPTION_EN || ''
+          : process.env.NEXT_PUBLIC_PROJECT_DESCRIPTION_ZH || ''}
       </p>
       <Link
         href={`/${lang}/docs/started`}
         className="text-fd-foreground font-semibold underline"
-        // style={{
-        //   position: 'absolute',
-        //   bottom: '30%',
-        //   left: '50%',
-        // }}
+        style={{
+          position: 'absolute',
+          bottom: '30%',
+          left: '50%',
+        }}
       >
-        <Circuit />
+        <Dots />
       </Link>
     </main>
   )
