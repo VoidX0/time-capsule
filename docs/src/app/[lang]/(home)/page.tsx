3b0e00bb1762cdb3d8285e24e27f974d1c73f455
VoidX0 <2982927629@qq.com>
2025-12-02 12:00:33 +0800

    Add loading animation component and replace dots loader in page

--- docs/src/app/[lang]/(home)/page.tsx
+++ docs/src/app/[lang]/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { i18n } from '@/lib/i18n'
 import Link from 'next/link'
-import { Dots } from '@/components/loader/dots'
+import { Ball } from '@/components/loader/ball'
 
 export const generateStaticParams = () =>
   i18n.languages.map((lang) => ({ lang }))
@@ -12,7 +12,7 @@ export default async function HomePage({
 }) {
   const { lang } = await params
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
+    <main className="flex flex-1 flex-col items-center justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">
         {process.env.NEXT_PUBLIC_PROJECT_NAME || ''}
       </h1>
@@ -24,13 +24,8 @@ export default async function HomePage({
       <Link
         href={`/${lang}/docs/started`}
         className="text-fd-foreground font-semibold underline"
-        style={{
-          position: 'absolute',
-          bottom: '30%',
-          left: '50%',
-        }}
       >
-        <Dots />
+        <Ball />
       </Link>
     </main>
   )
